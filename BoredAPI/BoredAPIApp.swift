088534009edc81b0d2c9ab1f40995bd0b85abe57
Luke Vereker <vereker.l@etoncollege.org.uk>
2023-03-22 09:56:37 +0000

    Initial Commit

---
+++ BoredAPI/BoredAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoredAPIApp.swift
+//  BoredAPI
+//
+//  Created by Vereker, Luke (PRKB) on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BoredAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
